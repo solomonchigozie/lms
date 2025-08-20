666db9aeca16b4287805bd067530ab6e1f88c314
solomon chigozie <solomon1chigozie@gmail.com>
2025-08-20 20:48:38 +0100

    update to course cards

--- client/src/components/students/CourseCard.jsx
+++ client/src/components/students/CourseCard.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { assets } from '../../assets/assets'
+import { AppContext } from '../../context/AppContext';
+import { Link } from 'react-router-dom';
 
-const CourseCard = () => {
+const CourseCard = ({course}) => {
+  
+  const {currency} = useContext(AppContext)
+  console.log(course)
+
   return (
-    <div>
-      <h1>Course Card</h1>
-    </div>
+    <Link to={'/course/' + course._id} onClick={()=>screenTo(0,0,)}
+      className='border border-gray-500/30 pb-6 overflow-hidden rounded-lg'
+    >
+      <img src={course.courseThumbnail} alt="" className='w-full'/>
+      <div className='p-3 text-left'>
+        <h3 className='text-base font-semibold '>{course.courseTitle}</h3>
+        <p className='text-gray-500'>{course.educator.name}</p>
+        <div className='flex items-center space-x-2'>
+          <p>4.5</p>
+          <div className='flex'>
+            {[...Array(5)].map((_, i)=>( <img key={i} src={assets.star} alt='' 
+            className='w-3.5 h-3.5'/>))}
+          </div>
+          <p className='text-gray-500'>22</p>
+        </div>
+        <p className='text-base font-semibold text-gray-800'>{currency}{(course.coursePrice - course.discount * course.coursePrice / 1000).toFixed}</p>
+      </div>
+    </Link>
   )
 }
 
--- client/src/components/students/CourseSection.jsx
+++ client/src/components/students/CourseSection.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom';
+import { AppContext } from '../../context/AppContext';
+import CourseCard from './CourseCard';
 
 const CourseSection = () => {
+  const {allCourses} = useContext(AppContext)
   return (
-    <div>
-      <h1>Course Sections</h1>
+    <div className='py-16 md:px-40 px-8'>
+      <h2 className='text-3xl font-medium text-gray-800'>Learn from the best</h2>
+      <p className='text-sm md:text-base text-gray-500 mt-3 mb-10 '>
+        Discover our top-rated courses across various categories. From coding and design 
+        to business and wellness, our course are crafted to deliver results.
+      </p>
+
+      <div>
+        {allCourses.slice(0,4).map((course, index)=> <CourseCard key={index} 
+        course={course} />)}
+      </div>
+
+      <Link to={'/course-list'} onClick={()=>scrollTo(0,0)}
+      className='text-gray-500 border border-gray-500/30 px-10 py-3 rounded'
+      >Show all Courses</Link>
     </div>
   )
 }
